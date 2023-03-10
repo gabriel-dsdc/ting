7fd753f0949cba2a0486df2639fb43235cd2b1fd
Gabriel dos Santos Duarte Carvalhães <gabriel.ds.duarte99@gmail.com>
2023-03-09 21:51:55 -0300

    feat: implemented FIFO queue

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,19 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
+        return self._data[-1]
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self._data.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0 or index >= self.__len__():
+            raise IndexError
+        return self._data[index]
