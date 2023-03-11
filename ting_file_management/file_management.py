a73c380e196904bc8447a63742f89433a6e93edc
Gabriel dos Santos Duarte Carvalhães <gabriel.ds.duarte99@gmail.com>
2023-03-11 18:14:13 -0300

    feat: file management txt importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,11 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+
+
+def txt_importer(path_file: str):
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+    try:
+        with open(file=path_file, mode="r") as file:
+            return file.read().splitlines()
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
