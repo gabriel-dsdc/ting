9479c85e827440894d0c8c423957160c5dfd72d3
Gabriel dos Santos Duarte Carvalhães <gabriel.ds.duarte99@gmail.com>
2023-03-11 23:57:59 -0300

    feat: process file

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+import sys
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+
+
+def process(path_file: str, instance: Queue):
+    for index in range(len(instance)):
+        if path_file == instance.search(index)["nome_do_arquivo"]:
+            return
+
+    text_list = txt_importer(path_file)
+    if text_list:
+        processed_data = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(text_list),
+            "linhas_do_arquivo": text_list
+        }
+        instance.enqueue(processed_data)
+        print(processed_data, file=sys.stdout)
 
 
 def remove(instance):
