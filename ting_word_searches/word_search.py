f2c8e07608aee92bb93289bbea0bcc9c0c124059
Gabriel dos Santos Duarte Carvalhães <gabriel.ds.duarte99@gmail.com>
2023-03-12 02:40:37 -0300

    feat: search by word in process list

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -2,7 +2,7 @@ from typing import Dict, List
 from ting_file_management.queue import Queue
 
 
-def exists_word(word: str, instance: Queue) -> List[Dict]:
+def exists_word(word: str, instance: Queue) -> List[Dict[str, str | List[Dict[str, int]]]]:
     result = []
     for index in range(len(instance)):
         occurrences = []
@@ -21,5 +21,20 @@ def exists_word(word: str, instance: Queue) -> List[Dict]:
     return result
 
 
-def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+def search_by_word(word: str, instance: Queue) -> List[Dict[str, str | List[Dict[str, int | str]]]]:
+    result = []
+    for index in range(len(instance)):
+        occurrences = []
+        process = instance.search(index)
+        for n, row in enumerate(process["linhas_do_arquivo"]):
+            if word.lower() in row.lower():
+                occurrences.append({"linha": n + 1, "conteudo": row})
+
+        if occurrences:
+            result.append({
+                "palavra": word,
+                "arquivo": process["nome_do_arquivo"],
+                "ocorrencias": occurrences
+            })
+
+    return result
