cb5a93114973c8f45c159e8ba7ce63128ebde937
Gabriel dos Santos Duarte Carvalhães <gabriel.ds.duarte99@gmail.com>
2023-03-12 02:15:46 -0300

    feat: check if word exists in process queue

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,24 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from typing import Dict, List
+from ting_file_management.queue import Queue
+
+
+def exists_word(word: str, instance: Queue) -> List[Dict]:
+    result = []
+    for index in range(len(instance)):
+        occurrences = []
+        process = instance.search(index)
+        for n, row in enumerate(process["linhas_do_arquivo"]):
+            if word.lower() in row.lower():
+                occurrences.append({"linha": n + 1})
+
+        if occurrences:
+            result.append({
+                "palavra": word,
+                "arquivo": process["nome_do_arquivo"],
+                "ocorrencias": occurrences
+            })
+
+    return result
 
 
 def search_by_word(word, instance):
